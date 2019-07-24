1cdb5a9b4ce909a6e4f8b2e4ebe3054b9cdf5e49
danielsantosf <daniel.santos@4yousee.com.br>
2019-07-24 14:42:52 -0300

    Functional react component

--- react-full-header/src/Main.js
+++ react-full-header/src/Main.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { headerStyle, containerStyle, subtitleStyle, titleStyle, videoStyle } from './Styles';
 
-const Component = ({ name }) => (
-    <h1>Hello {name}!</h1>
-);
-
-Component.propTypes = {
-    name: PropTypes.string.isRequired,
+const defaultProps = {
+    bgColor: '#ccc',
+    textColor: '#fff',
+    font: 'sans-serif',
+    bgImg: '',
+};
+const propTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+    font: PropTypes.string,
+    bgImg: PropTypes.string,
+    video: PropTypes.string,
 };
 
-export default Component;
+const FullHeader = ({ title, subtitle, bgColor, textColor, font, bgImg, video}) => {
+    const headerStyleCombined = {
+        ...headerStyle,
+        backgroundColor: bgColor,
+        backgroundImage: `url(${bgImg})`,
+        color: textColor,
+        fontFamily: font,
+    };
+
+    const component = (
+        <header style={headerStyleCombined}>
+            <div style={containerStyle}>
+                {title && <h1 style={titleStyle}>{title}</h1>}
+                {subtitle && <h2 style={subtitleStyle}>{subtitle}</h2>}
+            </div>
+            {video && <video autoPlay muted loop src={video} style= {videoStyle}/>}
+        </header>
+    );
+
+    return component;
+};
+
+
+FullHeader.propTypes = propTypes;
+FullHeader.defaultProps = defaultProps;
+
+export default FullHeader;
